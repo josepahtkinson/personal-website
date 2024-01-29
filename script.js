257b42e145ffd56cd362ac5c9a4d6ee3b2c59d10
Joseph <143543502+josepahtkinson@users.noreply.github.com>
2024-01-29 00:51:54 -0500

    Added main files for project structure

---
+++ script.js
@@ -0,0 +1,16 @@
+const sr = ScrollReveal ({
+    distance: '50px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.logo',{delay: 250, origin: 'left'})
+sr.reveal('.navbar',{delay: 350, origin: 'top'})
+sr.reveal('.menu-btn',{delay: 400, origin: 'right'})
+sr.reveal('.main-text span',{delay: 600, origin: 'top'})
+sr.reveal('.main-text h1',{delay: 700, origin: 'left'})
+sr.reveal('.main-text p',{delay: 750, origin: 'right'})
+sr.reveal('.main-btn',{delay: 800, origin: 'left'})
+sr.reveal('.main-text p',{delay: 850, origin: 'right'})
+sr.reveal('.socialmedias p',{delay: 900, origin: 'bottom'})
+sr.reveal('.social',{delay: 900, origin: 'bottom'})
